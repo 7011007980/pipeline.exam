e546deb98a8cf383662a6da9f2dd025205ebb214
7011007980 <singhsandeep0071997>
2019-03-29 13:48:16 +0530

    sum of two no.

---
+++ pythn/ssum.py
@@ -0,0 +1,15 @@
+pipeline {
+    agent { docker { image 'python:3.5.1' } }
+    stages {
+        stage('build') {
+            steps {
+                sh """
+                      a=5
+                      b=3
+                      c=a+b
+                      print(c)
+                      """
+            }
+        }
+    }
+}
